0ab42f1fafb3a0e9646a6d12da54cd17738ac3b4
Serhio Magpie <serdidg@gmail.com>
2018-09-27 19:09:41 +0300

    *

--- docs/src/js/custom.js
+++ docs/src/js/custom.js
@@ -1,8 +1,13 @@
-if(cm._baseUrl.indexOf('projectstagingserver.com') > -1){
+if(cm._baseUrl.indexOf('serdidg.github.io') > -1){
+    cm._baseUrl = [cm._baseUrl, '/QuickSilk-UI/docs/build'].join('/');
+}else if(cm._baseUrl.indexOf('projectstagingserver.com') > -1){
     cm._baseUrl = [cm._baseUrl, 'dev-1/quicksilk__ui/docs/build'].join('/');
 }else{
     cm._baseUrl = [cm._baseUrl, 'docs/build'].join('/');
 }
+
+cm._assetsUrl = cm._baseUrl;
+App._assetsUrl = cm._assetsUrl;
 
 App.LoginBox.prototype.setParams({
     'Com.Tooltip' : {
